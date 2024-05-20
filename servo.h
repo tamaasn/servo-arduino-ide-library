40c4d780a5ce080f14b544a8d8833c5b9204c11b
tamaasn <43458726+tamaasn@users.noreply.github.com>
2024-05-20 19:46:03 +0700

    Add files via upload

---
+++ servo.h
@@ -0,0 +1,25 @@
+class Servo{
+  public:
+    void use_pin(int pin);
+    void write(unsigned int angle);
+    int CurrentPin;
+    void Set_MinPulseWidth(int min);
+    void Set_MaxPulseWidth(int max);
+    int MinPulseWidth=544;
+    int MaxPulseWidth=2400;
+};
+
+void Servo::use_pin(int pin){
+  pinMode(pin , OUTPUT);
+  CurrentPin = pin;
+}
+
+void Servo::write(unsigned int angle){
+  if (angle > 180) angle=180;
+  int pulsewidth = map(angle , 0 , 180 , MinPulseWidth , MaxPulseWidth);
+  digitalWrite(CurrentPin , HIGH);
+  delayMicroseconds(pulsewidth);
+  digitalWrite(CurrentPin , LOW);
+  delayMicroseconds(20000 - pulsewidth);
+}
+
